fa1dc3e55e3aac0aaa293830a9b23f1277ec5b6e
Leonardo Capristo <leonardo.capristo@hotmail.com>
2024-08-26 11:47:22 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -3,14 +3,26 @@ function calculate() {
     const price30x20 = 50.00;
     const price60x40 = 80.00;
     const priceChapa = 50.00;
-    const priceCaixaCorreio = 100.00;
-    const priceFechadura = 75.00;
+    const priceCaixaCorreio = 50.00;
+    const priceFechadura = 150.00;
     const priceTinta = 80.00;
-    const priceSacosPeso = 20.00; // Adicione o preço do saco de peso
+    const priceSacosPeso = 30.00; // Adicione o preço do saco de peso
     const priceMotor = {
         motor4: 200.00,
         motor8: 250.00,
-        motor16: 300.00
+        motor16: 300.00,
+        x2motor4: 400,
+        x2motor8:500,
+        x2motor16:600
+    };
+
+    const motorNames = {
+        motor4: 'Motor 4 segundos',
+        motor8: 'Motor 8 segundos',
+        motor16: 'Motor 16 segundos',
+        x2motor4: '2x Motor 4 segundos',
+        x2motor8: '2x Motor 8 segundos',
+        x2motor16: '2x Motor 16 segundos'
     };
 
     // Preços dos kits
@@ -26,11 +38,7 @@ function calculate() {
         kit3: 'Kit 18x18'
     };
 
-    const motorNames = {
-        motor4: 'Motor 4 segundos',
-        motor8: 'Motor 8 segundos',
-        motor16: 'Motor 16 segundos'
-    };
+ 
 
     // Obtendo valores dos campos
     const bars30x20 = parseInt(document.getElementById('bars30x20').value) || 0;
@@ -43,11 +51,13 @@ function calculate() {
     // Custos adicionais fixos
     const totalCaixasCorreio = 1; // Valor fixo
     const totalFechaduras = 1; // Valor fixo
-
+    const totalPriceTinta = 1 ;
+    
     // Calculando o total de barras, chapas e custos fixos
     const totalBars30x20 = bars30x20 > 0 ? bars30x20 : 0;
     const totalBars60x40 = bars60x40 > 0 ? bars60x40 : 0;
     const totalChapas = chapa > 0 ? chapa : 0;
+    const totalSacosPesos = sacosPeso > 0 ? sacosPeso : 0;
     const totalCostBars30x20 = totalBars30x20 * price30x20;
     const totalCostBars60x40 = totalBars60x40 * price60x40;
     const totalCostChapas = totalChapas * priceChapa;
@@ -68,18 +78,27 @@ function calculate() {
                       totalCostSacosPeso + 
                       priceTinta; // Tinta é sempre incluída
 
+
+    // Despesas fixas como 3 dias do ajudante, agua e luz, materiais(arame de solda,flaps,gás)
+    const despesasFixas = 800
+
+    const totalVenda = (totalCost * 1.90) + despesasFixas;
+
     // Exibindo o resultado
     const resultElement = document.getElementById('result');
     resultElement.innerHTML = `
         ${totalBars30x20 > 0 ? `<p>Total de barras 30x20: ${totalBars30x20} (R$ ${totalCostBars30x20.toFixed(2)})</p>` : ''}
         ${totalBars60x40 > 0 ? `<p>Total de barras 60x40: ${totalBars60x40} (R$ ${totalCostBars60x40.toFixed(2)})</p>` : ''}
         ${totalChapas > 0 ? `<p>Total de chapas de ferro: ${totalChapas} (R$ ${totalCostChapas.toFixed(2)})</p>` : ''}
+        ${totalSacosPesos > 0 ?  `<p>Custo dos sacos de peso: ${sacosPeso} (R$ ${totalCostSacosPeso.toFixed(2)})</p>`: ''}
         ${totalCostKit > 0 ? `<p>Custo do Kit selecionado: ${kitNames[kit] || 'Não selecionado'} (R$ ${totalCostKit.toFixed(2)})</p>` : ''}
         ${totalCostMotor > 0 ? `<p>Custo do Motor selecionado: ${motorNames[motor] || 'Não selecionado'} (R$ ${totalCostMotor.toFixed(2)})</p>` : ''}
+
+        <h4>Custos adicionais: </h4>
         <p>Custo da tinta: R$ ${priceTinta.toFixed(2)}</p>
-        <p>Custo dos sacos de peso: ${sacosPeso} (R$ ${totalCostSacosPeso.toFixed(2)})</p>
         <p>Custo da caixa de correio: R$ ${totalCostCaixasCorreio.toFixed(2)}</p>
         <p>Custo da fechadura: R$ ${totalCostFechaduras.toFixed(2)}</p>
         <p class="total-cost">Custo total: R$ ${totalCost.toFixed(2)}</p>
+        <p class="total-cost">Valor total de venda: R$ ${totalVenda.toFixed(2)}</p>
     `;
 }
